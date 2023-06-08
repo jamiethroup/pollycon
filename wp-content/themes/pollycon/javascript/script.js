690974165b1001eacab7715f125fead7f64241fa
Jamie T <jamiethroup@gmail.com>
2023-06-08 19:29:10 +0100

    Initial Commit

---
+++ wp-content/themes/pollycon/javascript/script.js
@@ -0,0 +1,13 @@
+/**
+ * The JavaScript code you place here will be processed by esbuild, and the
+ * output file will be created at `../theme/js/script.min.js` and enqueued by
+ * default in `../theme/functions.php`.
+ *
+ * For esbuild documentation, please see:
+ * https://esbuild.github.io/
+ */
+import Alpine from 'alpinejs'
+ 
+window.Alpine = Alpine
+ 
+Alpine.start()
